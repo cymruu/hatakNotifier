dc18cb8a769f1fe0ba92f4edfb6a963b4226e92b
Filip Bachul <filip.bachul@euvic.pl>
2020-05-19 22:00:24 +0200

    default format

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 var request = require('request');
 var xml2js = require('xml2js');
 var fs = require('fs');
-var {Client,Wykop} = require('wypokjs');
+var { Client, Wykop } = require('wypokjs');
 var config = require('./config.js');
-var wykop = new Wykop({appkey: config.appkey, secret: config.secret});
-var client = new Client(wykop, {username: 'sokytsinolop', accountkey: config.connection, userkey: 'letmeloginonmyown'});
+var wykop = new Wykop({ appkey: config.appkey, secret: config.secret });
+var client = new Client(wykop, { username: 'sokytsinolop', accountkey: config.connection, userkey: 'letmeloginonmyown' });
 
-function getNewSubtitles(since, cb){
-  request('http://grupahatak.pl/rss/', (error, response, body)=>{
+function getNewSubtitles(since, cb) {
+  request('http://grupahatak.pl/rss/', (error, response, body) => {
     var newSubtitles = [];
     if (!error && response.statusCode == 200) {
-      xml2js.parseString(body, function(err, result){
-        if(err)throw err;
-        for(var i in result.rss.channel[0].item){
-          if(new Date(result.rss.channel[0].item[i].pubDate) > since){
+      xml2js.parseString(body, function (err, result) {
+        if (err) throw err;
+        for (var i in result.rss.channel[0].item) {
+          if (new Date(result.rss.channel[0].item[i].pubDate) > since) {
             newSubtitles.push(result.rss.channel[0].item[i]);
           }
         }
-       return cb(newSubtitles);
+        return cb(newSubtitles);
       });
     }
   });
 }
-function getTags(title){
-  var tag = '#'+title.toLowerCase().replace(/\W/g, '');
-  var tags = [tag, tag+'napisy'];
+function getTags(title) {
+  var tag = '#' + title.toLowerCase().replace(/\W/g, '');
+  var tags = [tag, tag + 'napisy'];
   return tags;
 }
-function addToWypok(newSubtitles){
-  if(newSubtitles.length==0){
+function addToWypok(newSubtitles) {
+  if (newSubtitles.length == 0) {
     return;
   }
   var entry = `Wydaliśmy nowe napisy do: \n`;
-  for(var i in newSubtitles){
+  for (var i in newSubtitles) {
     var tags = getTags(newSubtitles[i].title[0]);
-    entry+=`${newSubtitles[i].title[0]} - [${newSubtitles[i].description[0]}](${newSubtitles[i].link[0]})\n${tags.join(' ')}\n`;
+    entry += `${newSubtitles[i].title[0]} - [${newSubtitles[i].description[0]}](${newSubtitles[i].link[0]})\n${tags.join(' ')}\n`;
   }
-  entry+='\n#grupahatak #napisy #hatakbot';
-  client.request('entries/add', {postParams: {body: entry}}, (err, response)=>{
-      if(err){console.log(err);throw err;}
-      console.log(response);
-      updateLast(new Date(newSubtitles[0].pubDate[0]));
-      return true;
-});
+  entry += '\n#grupahatak #napisy #hatakbot';
+  client.request('entries/add', { postParams: { body: entry } }, (err, response) => {
+    if (err) { console.log(err); throw err; }
+    console.log(response);
+    updateLast(new Date(newSubtitles[0].pubDate[0]));
+    return true;
+  });
 }
-function getLastUpdate(){
-fs.readFile( __dirname + '/last.txt', function (err, data) {
-  if(err)throw err;
-  getNewSubtitles(new Date(data.toString()), addToWypok);
-});
+function getLastUpdate() {
+  fs.readFile(__dirname + '/last.txt', function (err, data) {
+    if (err) throw err;
+    getNewSubtitles(new Date(data.toString()), addToWypok);
+  });
 }
-function updateLast(lastUpdateTime){
-  fs.writeFile( __dirname + '/last.txt', new Date(lastUpdateTime), function(err) {
-    if(err)throw err;
-});
+function updateLast(lastUpdateTime) {
+  fs.writeFile(__dirname + '/last.txt', new Date(lastUpdateTime), function (err) {
+    if (err) throw err;
+  });
 }
-client.getUserKey().then((res)=>{
+client.getUserKey().then((res) => {
   getLastUpdate();
 });
